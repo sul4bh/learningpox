d50abdd39899bebb710614d0e3061f6955b6980d
brownbytes <c.vijaya.durga@outlook.com>
2013-10-22 18:51:39 +1100

    Create l2_switching_v3.py
    Descriptions:
    LLDP is blocked by switches which run 802.1 , since the control logic now resides in the controller , the controller by default considers the switch to be blocking and hence instructs the switch to drop the packet
    
    note to self : merge with existing code
    
    Testing:
    
    mininet@mininet-vm:~$ sudo mn --topo single,3 --mac --switch ovsk --controller remote
    *** Creating network
    *** Adding controller
    *** Adding hosts:
    h1 h2 h3 
    *** Adding switches:
    s1 
    *** Adding links:
    (h1, s1) (h2, s1) (h3, s1) 
    *** Configuring hosts
    h1 h2 h3 
    *** Starting controller
    *** Starting 1 switches
    s1 
    *** Starting CLI:
    
    mininet> dpctl dump-flows
    *** s1 ------------------------------------------------------------------------
    mininet>mininet> h1 ping h2
    PING 10.0.0.2 (10.0.0.2) 56(84) bytes of data.
    64 bytes from 10.0.0.2: icmp_req=1 ttl=64 time=0.230 ms
    64 bytes from 10.0.0.2: icmp_req=2 ttl=64 time=0.049 ms
    64 bytes from 10.0.0.2: icmp_req=3 ttl=64 time=0.056 ms
    64 bytes from 10.0.0.2: icmp_req=4 ttl=64 time=0.050 ms
    64 bytes from 10.0.0.2: icmp_req=5 ttl=64 time=0.126 ms
    64 bytes from 10.0.0.2: icmp_req=6 ttl=64 time=0.041 ms
    64 bytes from 10.0.0.2: icmp_req=7 ttl=64 time=0.129 ms
    64 bytes from 10.0.0.2: icmp_req=8 ttl=64 time=0.055 ms
    64 bytes from 10.0.0.2: icmp_req=9 ttl=64 time=0.055 ms
    ^C
    --- 10.0.0.2 ping statistics ---
    9 packets transmitted, 9 received, 0% packet loss, time 7999ms
    rtt min/avg/max/mdev = 0.041/0.087/0.230/0.060 ms
    mininet> dpctl dump-flows
    *** s1 ------------------------------------------------------------------------
    in_port(1),eth(src=00:00:00:00:00:01,dst=00:00:00:00:00:02),eth_type(0x0800),ipv4(src=10.0.0.1,dst=10.0.0.2,proto=1,tos=0,ttl=64,frag=no),icmp(type=8,code=0), packets:8, bytes:784, used:3.872s, actions:3,2,0
    in_port(2),eth(src=00:00:00:00:00:02,dst=00:00:00:00:00:01),eth_type(0x0800),ipv4(src=10.0.0.2,dst=10.0.0.1,proto=1,tos=0,ttl=64,frag=no),icmp(type=0,code=0), packets:8, bytes:784, used:3.872s, actions:3,0,1

---
+++ l2_switching_v3.py
@@ -0,0 +1,103 @@
+##################################
+#author -durga
+#controller - pox
+# l2 switching - transparent mode added
+# L2 controller inserts the flow table entries based on switching rules
+# the following code emulates basic source-mac address learning , forwarding
+# and filtering mechanisms found on switches. algorithm as below:
+# 1.switch forwards an unknown dest packet to the controller
+#   > check for the destination mac address , drop if the packet is a LLDP packet.
+#   > if the packet is a multicast packet, broadcast the same.
+#   > controller to check if (src-mac-add ,src-port-id) pair as in the recieved         packet exists in its addr table. 
+#        >a. if no entry , then make an entry
+#        >b. if an entry is made, dont change
+#   > controller to check for (des-mac-add,dest-port-id) pair in its addr table
+#       >a. if an entry is made, send relevant flow mod message to switch with i             formation of outgoing port.
+#       >b. if no entry is made, send a flow mod message to switch to broadcast              the packet for that destnation mac addr 
+#       >c. if destination port addr is same as source port addr drop the packet#   > send the ofmessage to the out.
+##################################
+# generic info
+# all mentioned events ex:ConnectionUp and PacketIn can be found in pox/openflow/__init__.py
+# all events have min of 3 attributes 1. connection 2.ofp (of packet which triggerd the event) 3.dpid
+#though there is no req for controller to maintain mac addr table, since the ft on switch expires its always a good idea to have a ref on controller -- macaddrtable{}
+###################################
+
+from pox.lib import *
+from  pox.core import core  # to import core module to register and hook the module
+#from  pox.lib import packet as pkt #to import modules from packet
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+
+
+#log = core.getLogger()
+
+class L2_switch (EventMixin):
+        print "in L2switch class"
+        def __init__(self,transparent):
+        # as soon as the module is hooked initialise..
+                self.macaddrtable = {}
+                self.listenTo(core.openflow)#listening to the events from core.openflow
+                self.transparent = transparent
+
+        def _handle_ConnectionUp (self,event):
+         # event handler for 'connectionup' events
+                print"connected to switch" # log a comment as soon as a connection is established
+
+        def _handle_PacketIn (self,event):
+         # event handler for incoming packets.check pox/openflow/__init__.py for infor on PacketIn(Event) class
+         # ofp represents the real openflow packet which triggered the event and is an event attribute.
+                parsedpkt = event.parsed
+                print type(parsedpkt)
+                inport = event.ofp.in_port
+                data = event.ofp.data
+                connection = event.connection
+                ethpkt = parsedpkt.find('ethernet')
+                print type(ethpkt)
+                dstmacaddr = ethpkt.dst
+                srcmacaddr = ethpkt.src
+        #       log.debug("packet parsed")
+
+        #checkng if srcmacadd and srcportin exists in mac table do nothing, else update the mac table using the class method updateMap
+                self.updateMap(inport,srcmacaddr)
+
+        # processing the packet
+                flowmsg = self.processPacket(dstmacaddr,inport,self.transparent)
+
+        # sending the flowmod message to the openflow switch
+                self.sendFlowMod(flowmsg,connection)
+
+        def updateMap(self,srcportin,srcmacaddr): # to update the mac-addr, port table
+                if srcmacaddr in self.macaddrtable.keys() and self.macaddrtable[srcmacaddr] != srcportin :
+                        self.macaddrtable[srcmacaddr] == srcportin
+                elif srcmacaddr not in self.macaddrtable.keys():
+                        self.macaddrtable[srcmacaddr] = srcportin
+
+        def processPacket(self,destmacaddr,srcport,transparent):# frame flow entries to forward the packets based on entries made in macaddrtable
+                LLDPMACLST = ['01:80:c2:00:00:0e','01:80:c2:00:00:03','01:80:c2:00:00:00']
+                msg = of.ofp_flow_mod() #of mod message to be sent to switch
+
+                if destmacaddr in LLDPMACLST and not transparent: # incase the dest mac is LLDP and switch is non-transparent , drop the packet
+                        msg.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
+
+                elif destmacaddr.is_multicast: # if mulicast packet, then flood
+                        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+
+                elif destmacaddr in self.macaddrtable.keys():
+                        destport = self.macaddrtable[destmacaddr] # if dest mac addr in macaddr table, update the dest id 
+                        if destport != srcport: # if destport is not same as srcport, set the port id in action ad dest port
+                                msg.actions.append(of.ofp_action_output(port=destport))
+                        elif desport == srcport: #elif destport is same as srcport drop the packet
+                                msg.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
+
+                elif destmacaddr not in self.macaddrtable.keys(): # if the destmac aaadr has no entry in macaddr table,bc the packet.
+                        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+                return msg
+
+
+        def sendFlowMod(self,msg,connection):
+                connection.send(msg)
+
+def launch():
+        print "in launch.."
+        core.registerNew(L2_switch,False) #registering the component to the core . all the attributes while intialisation can be passed after the module . False here refers that the switch is not a transparent switch
+
